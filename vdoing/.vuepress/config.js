146cc48bdbb34ae7e0b6a686e8a6fb6cb62ed12e
niaiwomattt <gongyong1990@126.com>
2021-06-15 22:22:19 +0800

    add 添加新文章

--- vdoing/.vuepress/config.js
+++ vdoing/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: '上士闻道，勤而行之；中士闻道，若存若亡；下士闻道，大笑之。不笑不足以为道。',
   theme: 'vdoing',
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'icon', href: `/logo.ico` }],
     ['meta', { name: 'keywords', content: '计算机，中医，政治'}]
   ],
   themeConfig: {
