dda4ab5099ab414d2b2ee726035154ba401b76b3
niaiwomattt <gongyong1990@126.com>
2021-06-26 17:56:40 +0800

    mod 删除各种插件

--- vdoing/.vuepress/config.js
+++ vdoing/.vuepress/config.js
@@ -16,22 +16,5 @@ module.exports = {
   },
   author: '上士之道',
   //  temp: 'docs',
-  dest: 'docs',
-  plugins: [
-    '@vuepress/google-analytics',
-    {
-      'ga': 'G-QZKQ19M0NV' //替换成自己实际申请的ID
-    },
-    "vuepress-plugin-comment",
-    {
-      choosen: "valine",
-      // options选项中的所有参数，会传给Valine的配置
-      options: {
-        el: "#valine-vuepress-comment",
-        appId: "3H7g9gRXWCdtl884PAjdkwQ6-MdYXbMMI",
-        appKey: "Eci42Dyalsq8ic4AfxVfM6O4",
-        path: '<%- frontmatter.to.path %>'
-      }
-    }
-  ]
+  dest: 'docs'
 }
